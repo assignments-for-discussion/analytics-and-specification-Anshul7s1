4868f4387d38647935291a765bfe4a417e5d83b6
Anshul7s1 <94890020+Anshul7s1@users.noreply.github.com>
2021-12-08 22:24:26 +0530

    Documented the changes

--- average.js
+++ average.js
@@ -1,6 +1,10 @@
 
 function average(numbers) {
   // return numbers.reduce((p, c)=> p + c, 0) / numbers.length;
+  
+  // the problem here is that many of the values in `numbers` array can be NaNs
+  // as expected from various real world sensors.
+  // So the solution is to filter out the NaNs and then calculate the average.
   const realNumbers = numbers.filter(num => !isNaN(num));
   const avg = realNumbers.reduce((p, c) => p+c, 0) / realNumbers.length;
   return avg;
