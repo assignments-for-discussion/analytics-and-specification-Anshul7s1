992199fe5043799163445112d49b1ae975065f6b
Anshul7s1 <94890020+Anshul7s1@users.noreply.github.com>
2021-12-08 22:20:12 +0530

    Update average.js

--- average.js
+++ average.js
@@ -1,6 +1,9 @@
 
 function average(numbers) {
-  return numbers.reduce((p, c)=> p + c, 0) / numbers.length;
+  // return numbers.reduce((p, c)=> p + c, 0) / numbers.length;
+  const real_numbers = numbers.filter(num => !isNaN(num));
+  const avg = real_numbers.reduce((p, c) => p+c, 0) / real_numbers.length;
+  return avg;
 }
 
 module.exports = {average};
